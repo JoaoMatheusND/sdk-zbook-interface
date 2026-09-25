640ca377b92f55d0e40145c8da8dfb241bf695f8
João Dias <joao.dias@edge.ufal.br>
2026-09-25 17:47:31 -0300

    chore: add the scaffold to project
    Signed-off-by: João Dias <joao.dias@edge.ufal.br>

--- interface/Kconfig.io
+++ interface/Kconfig.io
@@ -1,3 +1,9 @@
 menu "ZBook Interface - I/O"
 
+config ZBOOK_INTERFACE_IO
+    bool "Enable ZBook Interface I/O"
+    default n
+    help
+      Enable the ZBook Interface I/O module.
+
 endmenu
---
+++ interface/includes/example/zbook_sample.h
@@ -0,0 +1,16 @@
+/*******************************************************************
+ * @file zbook_sample.h
+ *
+ * @brief Description of the file.
+ * @author Dennis Ritchie (dennis.ritchie@edge.ufal.br)
+ * @version 0.1
+ * @date DD/MM/YYYY
+ *
+ * @copyright Copyright (c) 2026
+ *
+ *****************************************************************/
+
+ #ifndef ZBOOK_SAMPLE_H
+ #define ZBOOK_SAMPLE_H
+
+ #endif /* ZBOOK_SAMPLE_H */
